6e5aa230bd4e3a60db510814d20234788525d92a
Baptiste Gouhoury <gouhoury.baptiste@gmail.com>
2025-07-11 20:03:40 +0200

    add unit test s3 storage profile

--- pkg/apis/v1/storage/profile/s3_test.go
+++ pkg/apis/v1/storage/profile/s3_test.go
@@ -3,7 +3,44 @@ package profile
 import (
 	"encoding/json"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestS3StorageProfile(t *testing.T) {
+	profile := NewS3StorageSettings(
+		"bucket1",
+		"eu-west-1",
+		WithSTSEnabled(),
+		WithSTSRoleARN("role-arn"),
+		WithS3KeyPrefix("prefix"),
+		WithEndpoint("http://minio:9000/"),
+		WithS3AlternativeProtocols(),
+		WithAssumeRoleARN("assume"),
+		WithAWSKMSKeyARN("kms"),
+		WithFlavor(S3CompatFlavor),
+		WithPathStyleAccess(),
+		WithPushS3DeleteDisabled(true),
+		WithRemoteSigningURLStyle(VirtualHostSigningURLStyle),
+		WithSTSTokenValiditySeconds(7200),
+	)
+
+	assert.Equal(t, StorageFamilyS3, profile.GetStorageFamily())
+	assert.Equal(t, "bucket1", profile.Bucket)
+	assert.Equal(t, "eu-west-1", profile.Region)
+	assert.Equal(t, true, profile.STSEnabled)
+	assert.Equal(t, "role-arn", *profile.STSRoleARN)
+	assert.Equal(t, "prefix", *profile.KeyPrefix)
+	assert.Equal(t, "http://minio:9000/", *profile.Endpoint)
+	assert.Equal(t, true, *profile.AllowAlternativeProtocols)
+	assert.Equal(t, "assume", *profile.AssumeRoleARN)
+	assert.Equal(t, "kms", *profile.AWSKMSKeyARN)
+	assert.Equal(t, S3CompatFlavor, *profile.Flavor)
+	assert.Equal(t, true, *profile.PathStyleAccess)
+	assert.Equal(t, true, *profile.PushS3DeleteDisabled)
+	assert.Equal(t, VirtualHostSigningURLStyle, *profile.RemoteSigningURLStyle)
+	assert.Equal(t, int64(7200), *profile.STSTokenValiditySeconds)
+}
 
 func TestS3StorageProfile_Marshal(t *testing.T) {
 	sp := NewS3StorageSettings(
